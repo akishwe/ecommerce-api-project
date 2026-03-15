f741b390b5ec38e7eb4a417c4a5f2206cc9bb659
akishwe <avinashkishwe1@gmail.com>
2026-03-15 15:26:57 +0530

    Completed User Profile API

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from apps.users.views.auth_views import LoginAPIView, RegisterUserAPIView
+from apps.users.views.auth_views import LoginAPIView, RegisterUserAPIView, UserProfileAPIView
 
 urlpatterns = [
     path("register/", RegisterUserAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
-
+    path("profile/", UserProfileAPIView.as_view(), name="profile"),
 ]
--- apps/users/views/auth_views.py
+++ apps/users/views/auth_views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics
 from apps.users.models import User
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from apps.users.serializers.user_serializers import  UserRegistrationSerializer
 from apps.users.serializers.auth_serializers import LoginSerializer
+from apps.users.serializers.user_serializers import UserSerializer
+from rest_framework.response import Response
 
 
 class RegisterUserAPIView(generics.CreateAPIView):
@@ -16,3 +20,12 @@ class RegisterUserAPIView(generics.CreateAPIView):
 class LoginAPIView(TokenObtainPairView):
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
+
+
+class UserProfileAPIView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
